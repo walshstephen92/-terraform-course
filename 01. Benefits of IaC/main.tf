acdc9c9c55a3d49042d58f69465e255ab7b71ca8
Stephen Walsh <Stephen.Walsh@perspectum.com>
2024-10-20 21:03:26 +0100

    updates

--- 01. Benefits of IaC/main.tf
+++ 01. Benefits of IaC/main.tf
@@ -120,3 +120,42 @@ resource "aws_nat_gateway" "nat_gateway" {
     Name = "demo_nat_gateway"
   }
 }
+
+# Terraform Data Block - To Lookup Latest Ubuntu 20.04 AMI Image
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+# Terraform Resource Block - To Build EC2 instance in Public Subnet
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name = "Ubuntu EC2 Server"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-06b21ccaeff8cd686"
+  instance_type = "t3.micro"
+
+  subnet_id              = aws_subnet.public_subnets["public_subnet_1"].id
+  vpc_security_group_ids = ["sg-0d9b4384ea5443186"]
+
+  tags = {
+    "Terraform" = "true"
+  }
+}
