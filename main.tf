ef6c053438735042d0e6af06b05c7f04c7234470
Stephen Walsh <Stephen.Walsh@perspectum.com>
2024-10-31 09:12:21 +0000

    more

--- main.tf
+++ main.tf
@@ -168,3 +168,12 @@ resource "aws_subnet" "variables-subnet" {
     Terraform = "true"
   }
 }
+
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+}
+
+resource "local_file" "private_key_pem" {
+  content = tls_private_key.generated.private_key_pem
+  filename = "MyAWSKey.pem"
+}
--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,25 @@
 terraform {
   required_version = "1.5.7"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    http = {
+      source = "hashicorp/http"
+      version = "~> 3.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "~> 2.0"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
 }
